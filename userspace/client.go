91acb9cfc5949fc9714e00c293ef396010702f8e
Marco Guerri <gmarco.dev@gmail.com>
2016-07-24 18:16:49 +0200

    Added userspace scripts to test transfers

---
+++ userspace/client.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+        "net"
+        "os"
+        "fmt"
+        "time"
+        "syscall"
+        "flag"
+        "io/ioutil"
+        "crypto/md5"
+        )
+
+var ip = flag.String("ip", "", "Server IP")
+var port = flag.Int("port", 0, "Server port")
+var payload_path = flag.String("payload", "", "Path of the payload to be sent")
+var runs = flag.Int("runs", 1, "Number of times the payload should be send")
+
+func check(e error) {
+    if e != nil {
+        fmt.Fprintf(os.Stderr, "\n%s", e)
+    }
+}
+
+
+func main() {
+
+    flag.Parse()
+    
+    if *ip == "" || *port == 0 || *payload_path == "" {
+        fmt.Println("Error while parsing command line arguments. Usage:")
+        flag.PrintDefaults()
+        os.Exit(1)
+    }
+
+    ipaddr := net.ParseIP(*ip)
+
+    if ipaddr == nil {
+        fmt.Fprintf(os.Stderr, "IP address is not valid")
+        os.Exit(1)
+    }
+
+    fmt.Printf("Contacting server at %s:%d\n", ipaddr.String(), *port)
+    tcpaddr := net.TCPAddr{
+                IP:   ipaddr,
+                Port: *port,
+    }
+
+    var retry int = 3
+    var curr_run int = *runs
+
+    for {
+        
+        if curr_run == 0 {
+            break
+        }
+
+        conn, err := net.DialTimeout("tcp", tcpaddr.String(), time.Duration(2 * time.Second))
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "\n")
+            if operr, ok := err.(*net.OpError); ok {
+                if operr.Err == syscall.ECONNREFUSED {
+                    fmt.Fprintf(os.Stderr, "Connection refused. ")
+                    retry -= 1
+
+                    if retry == 0 {
+                        fmt.Fprintf(os.Stderr, "Will not retry...\n")
+                        os.Exit(1)
+                    }
+
+                    fmt.Fprint(os.Stderr, "Retrying...\n")
+                    time.Sleep(2 * time.Second)
+                    continue
+
+                 } else {
+                    fmt.Fprintf(os.Stderr, "Error while connection to server: %s (%d)\n", operr.Err, operr.Err)
+                    retry -= 1
+                    continue 
+                 }
+            }
+        }
+
+        /* Connection succeeded, reset retry counter */
+        retry = 3
+
+        /* Reading payload, calculating md5sum and sending everything off to
+         * the client */
+        data, err := ioutil.ReadFile(*payload_path)
+        check(err)
+        checksum := md5.Sum(data)
+        
+        _, err = conn.Write(checksum[:])
+        check(err)
+        _, err = conn.Write(data)
+        check(err)
+
+        fmt.Fprintf(os.Stderr, "\rRun: %d", *runs - curr_run + 1)
+        conn.Close()
+        curr_run -= 1
+
+    }
+}
---
+++ userspace/server.py
@@ -0,0 +1,115 @@
+import socket
+import sys
+import struct
+import errno
+import os
+import hashlib
+import signal
+import time
+from time import sleep
+
+PORT        = 8080
+BUFF_SIZE   = 4096
+
+run = True
+
+def sigterm_handler(*args):
+    sys.stderr.write("Got SIGTERM, exiting...\n")
+    run = False
+
+
+def recv_payload(conn):
+    """
+    Receive checksum and payload from a socket
+    """
+    data = ""
+    try:
+        expected_checksum = conn.recv(16)
+        while True:
+            chunk =  conn.recv(BUFF_SIZE)
+            if len(chunk) == 0:
+                # Connection closed, recv does raise EOF
+                break
+            data += chunk
+    except Exception, e:
+        raise 
+
+    return expected_checksum, data
+
+def write_payload(data, expected_checksum):
+    """
+    Write corrupted payload on disk
+    """
+    filename = "expected-%s-%s" % (expected_checksum.encode("hex"), time.strftime("%s"))
+    try:
+        f = open(os.path.join("/tmp", "samples", filename), "wb")
+        f.write(data)
+        f.close()
+    except Exception, e:
+        raise
+
+
+def run():
+    """
+    Main function which listens for incoming connections
+    """
+
+    global run
+    if(len(sys.argv) < 2):
+        sys.stderr.write("Please pass the IP to bind to")
+        sys.exit(1)
+
+    signal.signal(signal.SIGINT, sigterm_handler)
+    
+    if not os.path.exists("/tmp/samples"):
+        os.mkdir("/tmp/samples", 755)
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((sys.argv[1], PORT))
+    except Exception, e:
+        sys.stderr.write("Error while binding socket: %s\n" % str(e));
+        sys.exit(1)
+    
+    s.listen(1)
+    received = 0
+    corrupted = 0
+    while run:
+            
+            try:
+                conn, addr = s.accept()
+            except Exception, e:
+                if not (type(e) == socket.error and e.errno == errno.EINTR):
+                    sys.stderr.write("Error while listening on socket %s\n" % str(e))
+                run = False
+                continue
+
+            try:
+                expected_checksum, data = recv_payload(conn)
+            except Exception, e:
+                if not (type(e) == socket.error and e.errno == errno.EINTR):
+                    sys.stderr.write("Error while receiving payload from socket: %s\n" % str(e))
+                run = False
+                continue
+            
+            # Checking md5sum, if it does not match, write the payload on disk
+            checksum = hashlib.md5(data)
+            if(checksum.digest() != expected_checksum):
+                corrupted += 1 
+                try:
+                    write_payload(data, expected_checksum)
+                except Exception, e:
+                    sys.stderr.write("Could not write payload to disk: %s\n", str(e))
+            else:
+                received +=1
+            
+            sys.stderr.write("\rPacket received: %s, corrupted %s" % 
+                             (received, corrupted))
+    try: 
+        conn.close()       
+    except Exception, e:
+        pass
+
+if __name__ == '__main__':
+    run()
+
